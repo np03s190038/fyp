30098ed1b4d690a87aabd457f066b1069efc7214
np03s190038 <59077385+np03s190038@users.noreply.github.com>
2021-07-20 06:46:29 +0545

    Add files via upload

---
+++ dashboard/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import User, BusinessDetails
+
+
+admin.site.register(User)
+admin.site.register(BusinessDetails)
---
+++ dashboard/forms.py
@@ -0,0 +1,69 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from .models import BusinessDetails
+from django.forms import ModelForm
+
+class AddBusinessForm(forms.ModelForm):
+    class Meta:
+        model = BusinessDetails
+        fields = '__all__'
+
+class BusinessForm(forms.Form):
+    business_name = forms.CharField()
+    business_address = forms.CharField()
+    business_phone = forms.CharField()
+    business_category = forms.CharField()
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget= forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'is_vendor', 'is_user')
